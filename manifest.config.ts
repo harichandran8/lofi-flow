faa261e8f834f5286bc2bfb866682801ff1f4faa
harichandran8 <minichandranmini08175@gmail.com>
2026-09-20 01:09:36 +0530

    chore: clean project structure

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,6 +5,12 @@ export default defineManifest({
     name:'LofiFlow',
     description:'Transform the audio of your active browser tab into a smooth lofi experience.',
     version:'0.1.0',
+    icons:{
+        16: "icons/icon16.png",
+    32: "icons/icon32.png",
+    48: "icons/icon48.png",
+    128: "icons/icon128.png",
+    },
     action:{
         default_title:'LofiFlow',
         default_popup:'index.html'
