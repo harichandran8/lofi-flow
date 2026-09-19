0903b67c79b2db706a3b2b4bb6a2baee20ab353c
harichandran8 <minichandranmini08175@gmail.com>
2026-09-19 16:43:17 +0530

    feat:detect active browser tab

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,5 +1,39 @@
+
+
 console.log("LofiFlow service worker started");
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("LofiFlow extension installed");
 });
+
+chrome.runtime.onMessage.addListener((message,_sender,sendResponse)=>{
+    if(message.type === "GET_ACTIVE_TAB"){
+        chrome.tabs.query(
+            {
+                active:true,
+                currentWindow:true
+            },
+            (tabs)=>{
+                const tab = tabs[0];
+
+                if(!tab){
+                    sendResponse({
+                        success:false,
+                        error:"No active tab found",
+                    });
+
+                    return;
+                }
+                sendResponse({
+                    success:true,
+                    tab:{
+                        id:tab.id,
+                        title:tab.title??'unknown Tab',
+                        url:tab.url??"",
+                    },
+                })
+            }
+        )
+        return true;
+    }
+})
